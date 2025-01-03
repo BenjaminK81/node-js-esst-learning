6152c8782ee38706bca57e4e4d8190a816d0277a
GuS WebDevs <127965713+Gastro-Soul-Webdevs@users.noreply.github.com>
2025-01-03 16:59:29 +0100

    Express und Pug ins Hauptverzeichnis hinzugefügt

--- 07_Frameworks/express/index.js
+++ 07_Frameworks/express/index.js
@@ -1,18 +1,20 @@
 import express from "express";
 const server = express();
 
-server.set("view engine", "pug");
-server.set("views", "./views");
+server.set('view engine', 'pug'); // nach npm Installation von pug, können templates angelegt werden, wei bei Kapitel mit pug 
+server.set('views', './views');
 
-server.get("/pizza", (request, response) => {
-  response.render("pizza", {
-    title: "Pizza",
-    message: "Tolle Pizzas!",
-  });
+// eine Route für /pizza
+server.get('/pizza', (request, response) => {
+    response.render('pizza', { // unter views wird das template pizza.pug aufgerufen und optional Daten übergeben
+        title: "WhatsUp Pizza",
+        header1: "What a f****** good pizza!"
+    });
 });
 
-server.get("/hallo/:name", (request, response) => {
-  response.send(`Hallo ${request.params.name}!`);
+// eine Route für hallo, mit übergebene Parameter in der URL
+server.get('/hallo/:name', (request, response) => {
+    response.send(`Hallo ${request.params.name}`);
 });
 
 server.listen(3500);
