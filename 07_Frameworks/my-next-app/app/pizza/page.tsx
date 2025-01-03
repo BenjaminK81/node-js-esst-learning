289c912e88f767e1a383b05977505d5c1edf670c
GuS WebDevs <127965713+Gastro-Soul-Webdevs@users.noreply.github.com>
2025-01-03 16:30:00 +0100

    NextJs - Seiten-Routing und Datei-Routing

---
+++ 07_Frameworks/my-next-app/app/pizza/page.tsx
@@ -0,0 +1,11 @@
+import styles from "../page.module.css";
+
+export default function Pizza() {
+    return (
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <h1>Pizza-Seite</h1>
+            </main>
+        </div>
+    );
+}
